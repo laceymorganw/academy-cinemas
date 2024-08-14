2b43c12a6d4616da6f4724b39dfad8f841bc3957
Lacey Morgan <laceywoodbury@yahoo.com>
2024-08-14 10:59:05 -0500

    Add files via upload

---
+++ Jquery-Assignment-Base/javascript/javascript.js
@@ -0,0 +1,13 @@
+//Initialize Popovers
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+
+popoverTriggerList.forEach(function (element) {
+    var imgSrc = element.getAttribute('data-bs-img');
+    var content = "<img class='star-rating' src='" + imgSrc + "'>";
+    new bootstrap.Popover(element, {
+        content: content,
+        html: true,
+        trigger: 'hover'
+    });
+});
